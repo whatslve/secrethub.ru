1fa0480b06b72e430853a1dabfcbba09b0cb3af5
vlad <whatslve@gmail.com>
2025-11-24 12:01:50 +0300

    name space bug fix

--- backend/src/app/Http/Controllers/Auth/AuthController.php
+++ backend/src/app/Http/Controllers/Auth/AuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
-use app\Http\Controllers\Controller;
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use http\Client\Request;
 use Illuminate\Support\Facades\Auth;
